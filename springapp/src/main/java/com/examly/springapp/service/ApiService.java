c22c3727a84fe8d3f1acb2a5181935e09d1ecc34
Iamneo <techadmin@iamneo.ai>
2023-10-13 15:58:23 +0000

    Backup Commit

---
+++ springapp/src/main/java/com/examly/springapp/service/ApiService.java
@@ -0,0 +1,30 @@
+package com.examly.springapp.service;
+
+import com.examly.springapp.model.Laptop;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ApiService {
+    private final List<Laptop> laptops = new ArrayList<>();
+        
+            public boolean addLaptop(Laptop laptop) {
+                    return laptops.add(laptop);
+                        }
+                            
+                                public Laptop getLaptopById(int laptopId) {
+                                        for (Laptop laptop : laptops) {
+                                                    if (laptop.getLaptopId() == laptopId) {
+                                                                    return laptop;
+                                                                                }
+                                                                                        }
+                                                                                                return null; // Return null if laptop is not found
+                                                                                                    }
+                                                                                                        
+                                                                                                            public List<Laptop> getAllLaptops() {
+                                                                                                                    return laptops;
+                                                                                                                        }
+                                                                                                                        }
+                                                                                                                        
